817cc52f1840fac74655d3388dbdde91acfe9fac
Jamel Hammoud <devjamel@gmail.com>
2023-01-22 20:06:29 -0400

    Updated commands deploy

--- bot/botCommands.ts
+++ bot/botCommands.ts
@@ -2,7 +2,7 @@ import { REST } from '@discordjs/rest'
 import { SlashCommandBuilder } from '@discordjs/builders'
 import { Routes } from 'discord-api-types/v9'
 
-const apiToken = process.env.REACT_APP_BOT_KEY!
+const apiToken = process.env.BOT_KEY!
 const clientId = '1066192909109567489'
 const guildId = '796600091430354974'
 
@@ -15,7 +15,7 @@ const commands = [
 const deploy = async () => {
   try {
     const rest = new REST({ version: '9' }).setToken(apiToken)
-    await rest.put(Routes.applicationGuildCommands(clientId, guildId) as any, { body: commands })
+    await rest.put(Routes.applicationCommands(clientId) as any, { body: commands })
     
     console.log('Deployed Bot commands')
   }
