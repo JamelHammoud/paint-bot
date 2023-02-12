c1fe236ac60adf3d6551afa84e129c752faaa93a
Jamel Hammoud <devjamel@gmail.com>
2023-02-11 21:40:37 -0400

    Added more fonts

--- src/utils/theme/typeface.ts
+++ src/utils/theme/typeface.ts
@@ -1,4 +1,7 @@
 export const typeface = {
   sans:
-    '\'Mona-Sans\', ui-sans-serif, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Segoe UI Symbol", sans-serif, "Apple Color Emoji", "Segoe UI Emoji"'
+    '\'Mona-Sans\', ui-sans-serif, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Segoe UI Symbol", sans-serif, "Apple Color Emoji", "Segoe UI Emoji"',
+  drawn:
+    '\'Gooddog\', ui-sans-serif, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Segoe UI Symbol", sans-serif, "Apple Color Emoji", "Segoe UI Emoji"',
+  monospace: "'Cascadia', ui-monospace, monospace"
 }
