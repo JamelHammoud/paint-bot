6921b089597f47889a58a91bd6b1b032d58304c9
Jamel Hammoud <devjamel@gmail.com>
2023-02-23 13:13:09 -0400

    Persisted state of options

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,19 +1,25 @@
 import storage from 'redux-persist/lib/storage'
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2'
+import { persistStore, persistReducer, PersistConfig, StateReconciler } from 'redux-persist'
+import { createFilter } from 'redux-persist-transform-filter'
 import { configureStore, combineReducers } from '@reduxjs/toolkit'
-import { persistStore, persistReducer } from 'redux-persist'
 import { canvasReducer } from './slices'
 
 const reducers = combineReducers({
   canvas: canvasReducer
 })
 
-const persistConfig = {
+const subsetFilter = createFilter('canvas', ['showBackground', 'closeOnSend'])
+
+const persistConfig: PersistConfig<any, any, any> = {
   key: 'root',
-  whitelist: [],
-  storage
+  storage,
+  whitelist: ['canvas'],
+  transforms: [subsetFilter],
+  stateReconciler: autoMergeLevel2 as StateReconciler<any>
 }
 
-const persistedReducer = persistReducer(persistConfig, reducers)
+const persistedReducer = persistReducer(persistConfig, reducers) as typeof reducers
 
 export const store = configureStore({
   reducer: persistedReducer,
