a356b330020fbf0eef90b858ddf9efc9b35b13b1
Jamel Hammoud <devjamel@gmail.com>
2023-06-28 15:36:08 -0300

    Wrapped in try-catch

--- bot/bot.ts
+++ bot/bot.ts
@@ -67,70 +67,78 @@ app.listen(PORT, () => {
 })
 
 app.post('/send', (req, res) => {
-  const { image, pid, message } = req.body
+  try {
+    const { image, pid, message } = req.body
 
-  if (!image || !pid) {
-    throw new Error('Invalid parameters')
+    if (!image || !pid) {
+      throw new Error('Invalid parameters')
+    }
+
+    const secret = process.env.SECRET!
+    const cryptr = new Cryptr(secret)
+    const session = cryptr.decrypt(pid)
+
+    const { iid, uid, cid, tid } = JSON.parse(session)
+
+    if (!iid || !uid || !cid) {
+      throw new Error('Invalid PID')
+    }
+
+    const channel = bot.channels.cache.get(cid) as TextChannel
+    const guild = bot.guilds.cache.get(channel.guildId) as Guild
+
+    if (!channel) {
+      throw new Error('Could not find channel')
+    }
+
+    const uncompressed = inflate(image, { to: 'string' })
+    const sfbuff = Buffer.from(uncompressed.split(',')[1], 'base64')
+    const attachment = new AttachmentBuilder(sfbuff, { name: 'canvas.png' })
+    const word = specialWords[Math.floor(Math.random() * specialWords.length)]
+    const date = new Date().toLocaleString()
+
+    console.log(`${uid} just ${word} in #${channel?.name} (${guild?.name}) at ${date}. ${message}`)
+
+    channel.send({
+      content: `<@${uid}> ${word}:${message?.trim() ? `\n> ${message}` : ''}`,
+      reply: {
+        messageReference: tid
+      },
+      files: [attachment]
+    })
+
+    res.json({
+      status: 'ok'
+    })
+  } catch (err) {
+    console.error(err)
   }
-
-  const secret = process.env.SECRET!
-  const cryptr = new Cryptr(secret)
-  const session = cryptr.decrypt(pid)
-
-  const { iid, uid, cid, tid } = JSON.parse(session)
-
-  if (!iid || !uid || !cid) {
-    throw new Error('Invalid PID')
-  }
-
-  const channel = bot.channels.cache.get(cid) as TextChannel
-  const guild = bot.guilds.cache.get(channel.guildId) as Guild
-
-  if (!channel) {
-    throw new Error('Could not find channel')
-  }
-
-  const uncompressed = inflate(image, { to: 'string' })
-  const sfbuff = Buffer.from(uncompressed.split(',')[1], 'base64')
-  const attachment = new AttachmentBuilder(sfbuff, { name: 'canvas.png' })
-  const word = specialWords[Math.floor(Math.random() * specialWords.length)]
-  const date = new Date().toLocaleString()
-
-  console.log(`${uid} just ${word} in #${channel?.name} (${guild?.name}) at ${date}. ${message}`)
-
-  channel.send({
-    content: `<@${uid}> ${word}:${message?.trim() ? `\n> ${message}` : ''}`,
-    reply: {
-      messageReference: tid
-    },
-    files: [attachment]
-  })
-
-  res.json({
-    status: 'ok'
-  })
 })
 
 bot.on('interactionCreate', async interaction => {
-  if (!interaction.isCommand() && !interaction.isUserContextMenuCommand()) {
-    return
-  }
+  try {
+    if (!interaction.isCommand() && !interaction.isUserContextMenuCommand()) {
+      return
+    }
 
-  const { commandName, user, channel, channelId } = interaction
-  const targetId = (interaction as any)?.targetId
-  const channelName = (channel as any).name
+    const { commandName, user, channel, channelId } = interaction
+    const targetId = (interaction as any)?.targetId
+    const channelName = (channel as any).name
 
-  if (commandName !== 'paint' && !targetId) {
-    return
-  }
+    if (commandName !== 'paint' && !targetId) {
+      return
+    }
 
-  const button = createButton(interaction.id, user.id, channelId, channelName, targetId)
+    const button = createButton(interaction.id, user.id, channelId, channelName, targetId)
 
-  if (interaction.isRepliable()) {
-    await interaction.reply({
-      components: [button as any],
-      ephemeral: true
-    })
+    if (interaction.isRepliable()) {
+      await interaction.reply({
+        components: [button as any],
+        ephemeral: true
+      })
+    }
+  } catch (err) {
+    console.error(err)
   }
 })
 
